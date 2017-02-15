a633de87b0e0d923005d5456e4ffd54efa380085
smchow <sunita.m.chowdhury@gmail.com>
2017-02-15 13:13:14 -0500

    Initial commit

---
+++ bamazonCustomer.js
@@ -0,0 +1,48 @@
+
+var mysql      = require('mysql');
+var connection = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'root',
+  password : 'root',
+  database : 'bamazon_db'
+});
+ 
+connection.connect();
+ 
+connection.query('SELECT * from departments', function (error, results, fields){
+  if (error) throw error;
+  console.log(results);
+  for (var i =0 ; i < results.length ; i++){
+  	  console.log(results[i].department_name);
+  }
+});
+ 
+/*function insertIntoBeers(name, type, abv){
+  connection.query("INSERT INTO beers SET ?", {
+      name: name,
+      type: type,
+      abv: abv
+    }, function(err, res) { console.log('completed!')});
+}
+
+funtion deleteFromTable(id, table){
+	connection.query("DELETE FROM" + table + "WHERE ?", {
+      id: id
+     
+    }, function(err, res) { console.log('Delete completed!')});
+}
+}
+
+deleteFromTable ('1', 'beers');
+*/
+/*updateTable('name', 'Special Beer'  , '1' , 'breweries');
+
+function updateTable(col , value, id, table){
+var qryStr = 'UPDATE ' + table + 'SET ' + col + ' = '  + value + ' where id = ' + id;
+connection.query(qryStr, function (error, results, fields){
+  if (error) throw error;
+//update  breweries set name = where id =1 ; 
+});
+}*/
+//insertIntoBeers('mikes vodka', 'vodka beer', 200);
+connection.end();
---
+++ schema.sql
@@ -0,0 +1,40 @@
+/*
+
+To run this file, we do the following in our Terminal:
+
+1. Go to the directory of this sql file.
+
+2. Get into our mysql console.
+
+3. Run "source schema.sql"
+
+*/
+
+-- Create the database  and specified it for use.
+CREATE DATABASE bamazon_db;
+USE bamazon_db;
+
+-- Create the table actors.
+CREATE TABLE departments (
+  id int AUTO_INCREMENT,
+  department_name varchar(30) NOT NULL,
+  over_head_costs int NOT NULL,
+  PRIMARY KEY(id)
+);
+
+-- Insert a set of records.
+INSERT INTO departments (department_name, over_head_costs) VALUES ("Appliances",  5000);
+INSERT INTO departments (department_name, over_head_costs) VALUES ("Baby", 1000);
+INSERT INTO departments (department_name, over_head_costs) VALUES ("Clothing", 2500);
+INSERT INTO departments (department_name, over_head_costs) VALUES ("Health", 4000);
+
+
+CREATE TABLE products (
+  id int AUTO_INCREMENT,
+  product_name varchar(30) NOT NULL,
+  department_id int,
+  price decimal(10,2) NOT NULL,
+  stock_quantity int,
+  PRIMARY KEY(id),
+  FOREIGN KEY(department_id) REFERENCES departments(id)
+);
